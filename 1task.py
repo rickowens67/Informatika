46bbe94d2b0df1196bd2cdb73738fa882944dcad
rickowens67 <evgrafov.ai@edu.spbstu.ru>
2026-04-07 13:39:43 +0300

    Лабораторная работа 3

---
+++ 1task.py
@@ -0,0 +1,16 @@
+# TODO Напишите функцию для поиска индекса товара
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    if find_item in items_list:
+        index_item = items_list.index(find_item)
+    else:
+        index_item = None
+          # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
+
